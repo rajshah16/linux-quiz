9b489bd7d883efff237c56c5270d7a40dc4a8f8a
= <=>
2021-04-06 22:30:37 +0530

    This is the first commit

---
+++ questions.js
@@ -0,0 +1,87 @@
+var questions = [
+    {
+        title: "What is SSL",
+        choices: ["SSL (Secure Sockets Layer) is a protocol for establishing authenticated and encrypted links between networked computers", 
+        "Transport Layer Security, and its now-deprecated predecessor which is not used effectively", 
+        " It is used for secure communication over a computer network, and is widely used on the Internet", 
+        "None of the above"],
+        answer: "SSL (Secure Sockets Layer) is a protocol for establishing authenticated and encrypted links between networked computers"
+    },
+    {
+        title: "What are keys?",
+        choices: ["binding the identities of entities such as websites and companies to cryptographic key pairs via digital documents", 
+        "They are used to establish and identify relationships between tables and also to uniquely identify any record or row of data inside a table.", 
+        "private and public keys", 
+        "a key is one of the keyboard's buttons. Letters, numbers, functions, and symbols are all represented on keys."],
+        answer: "binding the identities of entities such as websites and companies to cryptographic key pairs via digital documents"
+    },
+    ///etc.
+    {
+        title: "What are certificates?",
+        choices: ["Certification is the formal attestation or confirmation of certain characteristics of an object, person, or organization", 
+        "They're relatively small and contain details such as their date of issue and expiry", 
+        "It is used to keep private key secure and public key can be widely distributed", 
+        "all of the above"],
+        answer: "It is used to keep private key secure and public key can be widely distributed"
+    },
+    {
+        title: "What are handshakes",
+        choices: ["Greeting or tradition between the people ", 
+        "It is foundation of secure web browsing and electronic commerce", 
+        "It establishes the protocols of a communication link", 
+        "Used for authentication and secured links"],
+        answer: "It is foundation of secure web browsing and electronic commerce"
+    },
+    {
+        title: "Explain Authenticity with respect to certificates",
+        choices: ["The possession in which private key that matches the  public key in the certificate", 
+        "Different systems may require different types of credentials to ascertain a user's identity", 
+        "Authentication technology provides access control for systems by checking to see if a user's credentials match the credentials in a database", 
+        "All of the Above"],
+        answer: "The possession in which private key that matches the  public key in the certificate"
+    },
+    {
+        title: "Explain Integrity",
+        choices: ["It refers to the complete network as a whole with network-discoverable resources matching that listed in inventory", 
+        "the quality of having strong ethical principles that are followed at all times.", 
+        "refers to the accuracy and completeness of data.", 
+        "Documents signed by the certificate (e.g. web pages) have not been altered in transit by a man in the middle."],
+        answer: "Documents signed by the certificate (e.g. web pages) have not been altered in transit by a man in the middle."
+    },
+    {
+        title:"What is TLS?",
+        choices:["It is cryptographic protocol that uses explicit connections to establish secure communication between web server and client",
+        "The SSL and TLS are the same thing ",
+        "It is the successor to the SSL protocol for authentication and encryption",
+        "None of the above"
+        ],
+        answer:"It is the successor to the SSL protocol for authentication and encryption"
+    },
+    {
+        title:"Is it possible to login to SSH server without password?",
+        choices:["Yes but we have to use the a remote ssh server",
+        "It is a threat to the security",
+        "We need to use the ssh-keygen technology to create the public and private keys",
+        "none of the above"
+        ],
+        answer:"We need to use the ssh-keygen technology to create the public and private keys"
+    },
+    {
+        title:"What is the most common thing SSl is used for?",
+        choices:["The most common and well-known use of SSL/TLS is secure web browsing via the HTTPS protocol",
+        "A properly configured public HTTPS website includes an SSL/TLS certificate that is signed by a publicly trusted CA.",
+        "SSl is used to encrypt the private and public keys",
+        "none of the above"
+        ],
+        answer:"The most common and well-known use of SSL/TLS is secure web browsing via the HTTPS protocol"
+    },
+    {
+        title:"What is a Network Firewall?",
+        choices:["It provides a secure tunnel that protects your data from any intrusion.",
+        "It provides a solution of integrity and confidentiality to end customers",
+        "It filters traffic based on the configuration set by the firewall administrator",
+        "Firewall just checks the false network and diminishes it "
+        ],
+        answer:"It filters traffic based on the configuration set by the firewall administrator"
+    }
+];
